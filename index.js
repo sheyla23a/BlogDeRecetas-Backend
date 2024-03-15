01ebe67f9c4be1dd24647d79270d3b46d73eeed3
sheyla astorga <sheylaastorga1998@gmail.com>
2024-03-15 17:15:16 -0300

    tomo el puerto de una variable de entorno, caso contrario uso mi puerto 4000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 console.log('Hola Mundo')
 
 const app = express();
-app.set('port',process.env.PORT ||4000);
+app.set('port',process.env.PORT || 4000);
 app.listen(app.get('port'),()=>{
     console.log('Estoy en el puerto '+ app.get('port'))
 })
