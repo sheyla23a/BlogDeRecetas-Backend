b462b0bc3e2d2ddec774d899da26776ddc9fdfed
sheyla astorga <sheylaastorga1998@gmail.com>
2024-03-16 19:05:44 -0300

    aca agregue la logica correspondiente para obtener un arrays de recetas

--- src/controllers/recetas.controllers.js
+++ src/controllers/recetas.controllers.js
@@ -1,8 +1,14 @@
 import Receta from "../database/model/receta.js";
 
-export const listarRecetas = (req, res) => {
-  console.log("desde listar recetas");
-  res.send("Enviar lista de recetas...");
+export const listarRecetas = async(req, res) => {
+    try{
+      const recetas = await Receta.find()
+      res.status(200).json(recetas);
+
+    }catch(error){
+     console.log(error)
+     res.status(404).json({mensaje: 'No se encontro la lista de recetas'})
+    }
 };
 
 export const crearReceta = async (req, res) => {
