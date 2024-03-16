dc56a6762a15f85d763133ca1895fb727962bfaa
sheyla astorga <sheylaastorga1998@gmail.com>
2024-03-16 18:14:37 -0300

    agregue la funcion crearReceta haciendo mi conexion con postman para crear una nueva receta y darle de alta

--- src/controllers/recetas.controllers.js
+++ src/controllers/recetas.controllers.js
@@ -1,6 +1,22 @@
- import Receta from "../database/model/receta.js";
- 
- export const listarRecetas=(req,res)=>{
-    console.log('desde listar recetas');
-    res.send('Enviar lista de recetas...')
-}
+import Receta from "../database/model/receta.js";
+
+export const listarRecetas = (req, res) => {
+  console.log("desde listar recetas");
+  res.send("Enviar lista de recetas...");
+};
+
+export const crearReceta = async (req, res) => {
+  try {
+    console.log(req.body);
+    const recetaNueva = new Receta(req.body);
+    await recetaNueva.save();
+    res.status(201).json({
+      mensaje: "La receta fue creada correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "No se pudo procesar la solicitud de crear receta",
+    });
+  }
+};
--- src/routes/recetas.routes.js
+++ src/routes/recetas.routes.js
@@ -1,8 +1,8 @@
 import { Router} from "express";
-import {listarRecetas} from "../controllers/recetas.controllers.js"
+import {crearReceta, listarRecetas} from "../controllers/recetas.controllers.js"
 
 const router = Router();
 
-router.route('/recetas').get(listarRecetas)
+router.route('/recetas').get(listarRecetas).post(crearReceta)
 
 export default router;
